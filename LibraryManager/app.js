b3bd50c273d543209f32747e62a32d15abb6e53c
Kevin J Donohue <kevinjdonohue@yahoo.com>
2016-06-15 23:04:08 -0700

    Created LibraryManager Project
    Created sample app.ts

---
+++ LibraryManager/app.js
@@ -0,0 +1,8 @@
+var HelloWorld = (function () {
+    function HelloWorld(message) {
+        this.message = message;
+    }
+    return HelloWorld;
+}());
+var hello = new HelloWorld('Hello TypeScript');
+console.log(hello.message);
---
+++ LibraryManager/app.ts
@@ -0,0 +1,7 @@
+class HelloWorld {
+  constructor(public message: string) {}
+}
+
+var hello = new HelloWorld('Hello TypeScript');
+
+console.log(hello.message);
